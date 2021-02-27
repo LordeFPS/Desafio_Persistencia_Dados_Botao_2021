1cf4aa86e844da5cd1b370874a915992727075d2
lordefps <lodizinho_mice@hotmail.com>
2021-02-26 23:20:41 -0300

    Cariando ListaPessoa

---
+++ Cadastro_Geral/src/controller/ListaPessoa.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import entidade.Pessoa;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lorde
+ */
+public class ListaPessoa {
+    ArrayList<Pessoa> pessoas = new ArrayList();
+    
+    
+}
